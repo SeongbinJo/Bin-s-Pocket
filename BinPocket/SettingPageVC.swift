5647eb0db3e374ad9914b745772774ea3e5fa599
Jo SeongBin <avan31017@naver.com>
2023-02-16 21:00:27 +0900

    BinPocket Ver 1.0.3 도움말 중복 수정.

---
+++ BinPocket/SettingPageVC.swift
@@ -0,0 +1,8 @@
+//
+//  SettingPageVC.swift
+//  BinPocket
+//
+//  Created by 조성빈 on 2023/02/16.
+//
+
+import Foundation
---
+++ BinPocket/components/SegementBorder.swift
@@ -0,0 +1,20 @@
+//
+//  SegementBorder.swift
+//  BinPocket
+//
+//  Created by 조성빈 on 2023/02/16.
+//
+
+import UIKit
+
+class SegementBorder: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
